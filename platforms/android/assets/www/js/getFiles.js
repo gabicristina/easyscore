f5e98f7a35275a3b537934f74b6a21da0f566450
gabicristina <gaabi.cristina@gmail.com>
2014-06-03 16:48:25 -0300

    Tela inicial
    Incluídas alterações para criação da pasta "EasyScore" no dispositivo e
    inclusão dos arquivos contidos na pasta na lista da página inicial.

--- platforms/android/assets/www/js/getFiles.js
+++ platforms/android/assets/www/js/getFiles.js
@@ -3,19 +3,67 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
     alert("ready");
-    window.requestFileSystem(window.TEMPORARY, 5*1024*1024, gotFS, fail);
-    //window.requestFileSystem(LocalFileSystem.PERSISTENT, 1024*1024, onInitFs, errorHandler);
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 5*1024*1024, onFSSuccess, fail);
+    //window.requestFileSystem(LocalFileSystem.PERSISTENT, 5*1024*1024, gotFS, fail);
+    //window.requestFileSystem(window.TEMPORARY, 1024*1024, onInitFs, errorHandler);
 }
 
 function gotFS(fileSystem) {
    alert("got fs");
-   alert(fileSystem.root.fullPath);
+   alert(fileSystem.root.fullPath); //print /
+   var root = new DirectoryEntry(fileSystem.root.fullPath);
+	
+   //Get a directory reader
+   var directoryReader = root.createReader();
+
+   directoryReader.readEntries(gotFiles,fail);	
 }
 
+//The directory path is without the prefix of the root directory!
+function onFSSuccess(fileSystem) {
+	fileSystem.root.getDirectory("EasyScore", {create: true, exclusive: false}, doDirectoryListing, fail);
+}
+
+//dirEntry needs to be a parameter of the function!
+function doDirectoryListing(dirEntry) {
+	var directoryReader = dirEntry.createReader();
+	directoryReader.readEntries(gotFiles, FileError);
+}
+
+function gotFiles(entries) {
+	for(var i=0,len=entries.length; i<len; i++) {
+		//Name of the files
+		alert(entries[i].name);
+		$('ul').append($('<li/>', { //here appending `<li>`
+			'data-role' : "list-divider"
+		}).append($('<a/>', { //here appending `<a>` into `<li>`
+			'href' : '#musica_info',
+			'data-transition' : 'fade',
+			'text' : entries[i].name
+		})));
+	}
+	$('ul').listview('refresh');
+}
 function fail() {
 	alert("fail");
 }
 /*
+ * 
+
+http://www.raymondcamden.com/index.cfm/2014/2/17/Cordova-File-System--Important-Update
+http://www.html5rocks.com/en/tutorials/file/filesystem/ --DOCUMENTAÇÃO HTML
+https://gist.github.com/rosshanney/9060654 --'temp.wav'
+https://gist.github.com/devgeeks/4982983 --example-download-and-open.js
+http://stackoverflow.com/questions/20836040/window-requestfilesystem-is-undefined-in-ver-3-3-0
+
+SOMENTE PARA LINKS: http://stackoverflow.com/questions/21941731/cordova-phonegap-open-downloaded-file-inappbrowser
+
+http://docs.phonegap.com/en/1.4.1/phonegap_file_file.md.html
+https://developer.mozilla.org/en-US/docs/Web/API/DirectoryEntry
+https://github.com/apache/cordova-plugin-file/blob/master/doc/index.md --PLUGIN
+http://cordova.apache.org/docs/en/2.5.0/cordova_file_file.md.html --DOCUMENTAÇÃO PHONEGAP (ANTIGA)
+https://cordova.apache.org/docs/en/3.0.0/cordova_file_file.md.html#DirectoryEntry
+
 function onInitFs(fs) {
 
   fs.root.getFile('log.txt', {create: true, exclusive: true}, function(fileEntry) {
@@ -91,7 +139,7 @@ function getFiles() {
 	var directoryReader = parentEntry.createReader();
 
 	directoryReader.readEntries(success,fail);	
-}*/
+}
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
